56864875ce8fcb98d940c86b9f0bc1f3f8919076
JASAHS <jasahshamsudheen10@gmail.com>
2020-02-22 11:57:49 -0500

    Updated Play.ts file Updated Game.ts file

--- Scripts/scenes/Play.js
+++ Scripts/scenes/Play.js
@@ -16,7 +16,6 @@ var scenes;
 (function (scenes) {
     var Play = /** @class */ (function (_super) {
         __extends(Play, _super);
-        // PRIVATE INSTANCE MEMBERS
         // PUBLIC PROPERTIES
         // CONSTRUCTOR
         function Play() {
@@ -28,11 +27,30 @@ var scenes;
         // PUBLIC METHODS
         //initialize and instatiate
         Play.prototype.Start = function () {
+            //instantiate a new Text object
+            // buttons
+            this._diceroll01 = new objects.Button(config.Game.ASSETS.getResult("3"), 200, 240, true);
+            this._diceroll02 = new objects.Button(config.Game.ASSETS.getResult("4"), 400, 240, true);
+            this._rollButton = new objects.Button(config.Game.ASSETS.getResult("rollButton"), 320, 430, true);
             this.Main();
         };
         Play.prototype.Update = function () {
         };
         Play.prototype.Main = function () {
+            var _this = this;
+            this.addChild(this._diceroll01);
+            this.addChild(this._diceroll02);
+            this.addChild(this._rollButton);
+            this._rollButton.on("click", function () {
+                // let ranNum01=Math.floor(Math.random() * 6) + 1  ;
+                // let ranNum02=Math.floor(Math.random() * 6) + 1  ;
+                var ranNum01 = 2;
+                var ranNum02 = 5;
+                console.log(ranNum02);
+                console.log(ranNum01);
+                _this._diceroll01 = new objects.Button(config.Game.ASSETS.getResult(ranNum01), 200, 240, true);
+                _this._diceroll02 = new objects.Button(config.Game.ASSETS.getResult(ranNum02), 400, 240, true);
+            });
         };
         return Play;
     }(objects.Scene));
--- Scripts/scenes/Play.ts
+++ Scripts/scenes/Play.ts
@@ -3,7 +3,9 @@ module scenes
     export class Play extends objects.Scene
     {
         // PRIVATE INSTANCE MEMBERS
- 
+        private _rollButton: objects.Button;
+        private _diceroll01: objects.Button;
+        private _diceroll02: objects.Button;
 
         // PUBLIC PROPERTIES
 
@@ -22,23 +24,47 @@ module scenes
         //initialize and instatiate
         public Start(): void 
         {
-            
+              //instantiate a new Text object
+              
+              // buttons
+               this._diceroll01 = new objects.Button(config.Game.ASSETS.getResult("3"), 200, 240, true);
+               this._diceroll02 = new objects.Button(config.Game.ASSETS.getResult("4"), 400, 240, true);
+               this._rollButton=new objects.Button(config.Game.ASSETS.getResult("rollButton"), 320, 430, true);
+              this.Main();
 
             
-             this.Main();
+            
         }        
         
         public Update(): void 
         {
+       
 
         }
         
         public Main(): void 
         {
+            this.addChild(this._diceroll01);
 
+        
+            this.addChild(this._diceroll02);
+            this.addChild(this._rollButton);
+
+             this._rollButton.on("click", ()=>{
+                     // let ranNum01=Math.floor(Math.random() * 6) + 1  ;
+            // let ranNum02=Math.floor(Math.random() * 6) + 1  ;
+            let ranNum01=2;
+            let ranNum02=5;
+            console.log(ranNum02);
+            console.log(ranNum01);
+            this._diceroll01 = new objects.Button(config.Game.ASSETS.getResult(ranNum01), 200, 240, true);
+            this._diceroll02 = new objects.Button(config.Game.ASSETS.getResult(ranNum02), 400, 240, true);
+            }); 
+            
 
         }
 
         
     }
 }
+
